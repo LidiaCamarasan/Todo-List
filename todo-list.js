5fc7eb2e6b8e2d161524892203fc6816293d21fc
LidiaLidia <lidia_mv@yahoo.com>
2024-02-19 00:41:02 +0200

    addEventListener

--- todo-list.js
+++ todo-list.js
@@ -12,20 +12,29 @@ const todoList = [
 const renderTodoList = () => {
   let todoListHTML = "";
 
-  todoList.forEach(function (todoObject, index) {
+  todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
     const html = `<div> ${name}</div>
     <div> ${dueDate} </div>
-    <button onclick="
-        todoList.splice(${index}, 1); 
-        renderTodoList();
-    " class="delete-todo-button">Delete</button>`;
+    <button class="delete-todo-button js-delete-todo-button">Delete</button>`;
     todoListHTML += html;
   });
 
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+  document
+    .querySelectorAll(".js-delete-todo-button")
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener("click", () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 };
 renderTodoList();
+
+document.querySelector(".js-todo-button").addEventListener("click", () => {
+  addTodo();
+});
 
 const addTodo = () => {
   const inputElement = document.querySelector(".js-name-input");
